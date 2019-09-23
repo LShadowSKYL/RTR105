13650e8b50ac71bb1122258b7f8d74511942c2fe
user <raitis553@inbox.lv>
2019-09-23 18:12:59 +0300

    20190923_18_12

---
+++ darbi/02.c
@@ -0,0 +1,5 @@
+#include<stdio.h>
+
+void main() {
+ printf("Sveiks!\n Ar tevi ir Google\?\t\n");
+ }
--- darbi/hello.c
+++ darbi/hello.c
@@ -1,10 +1,9 @@
-#include<studio.h>
+#include<stdio.h>
 
-void main()
- {
+void main() {
 // Hello World! // neder
 // "Hello World!" // neder
 // "Hello World!"; // der, bet teksts neparādās
 // printf("Hello World!"); // der, bet nas smuki
- printf("Hello World!");
+ printf("Hello World!\n");
  }
