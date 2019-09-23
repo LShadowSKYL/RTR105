d6632c1892cea2cb0b81a2acdb6b404c1c61d2b9
user <raitis553@inbox.lv>
2019-09-23 16:12:15 +0300

    20190923_16_11

---
+++ darbi/hello.c
@@ -0,0 +1,10 @@
+#include<studio.h>
+
+void main()
+ {
+// Hello World! // neder
+// "Hello World!" // neder
+// "Hello World!"; // der, bet teksts neparādās
+// printf("Hello World!"); // der, bet nas smuki
+ printf("Hello World!");
+ }
