516f10d1def8cb25dd407dc5ec2f6ee0f9805304
user <raitis553@inbox.lv>
2019-12-09 15:57:04 +0200

    20190912_15_57

---
+++ darbi/Items.c
@@ -0,0 +1,8 @@
+1. char, int, float,... - mainīgie.
+2. printf, scanf - dialogs ar lietotaaju.
+3. =, +, -, orepātors.
+4. if nosac. operators.
+5. for, while,... cikls.
+6. lietotāja funkcijas.
+7. fprintf, fscanf - darbs ar failiem (cplusplus,com) + fopen, fseek, fclose
+8. pointeri un masivi. 
